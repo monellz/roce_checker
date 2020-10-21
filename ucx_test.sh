d5f7d5facc03ca20f49316a5c8a5bc3b6c6d2781
monellz <monell.zhong@gmail.com>
2020-10-21 14:22:55 +0800

    ucx_test

--- ucx_test.sh
+++ ucx_test.sh
@@ -29,20 +29,41 @@ ssh ${IP2} \
 
 # client side
 
-ssh ${IP1} > ${OUTPUT_FILE} 2>/dev/null << remotessh
+ssh ${IP1 \
+"cd ${TARGET_DIR};" \
+"source env_load.sh ${IP1} ${TARGET_DIR};" \
+'pkill ucx_perftest;' \
+'export UCX_NET_DEVICES=${DEV}:${V2_PORT};' \
+'sleep 5;' \
+"ucx_perftest -b test_types_ucp -p ${PORT} ${IP2};" \
+'exit' > ${OUTPUT_FILE} 2>&1 
 
-cd ${TARGET_DIR}
-source env_load.sh ${IP1} ${TARGET_DIR}
+#ssh ${IP1} > ${OUTPUT_FILE} 2>/dev/null << remotessh
+#
+#cd ${TARGET_DIR}
+#source env_load.sh ${IP1} ${TARGET_DIR}
+#
+## try 3 times for connection
+#for i in {1..3}
+#do
+#    sleep 5
+#    ucx_perftest -p ${PORT} -b test_types_ucp ${IP2} 2>&1
+#    if [[ $? -eq 0 ]]; then
+#        exit 0
+#    fi
+#done
+#
+#exit 1
+#remotessh
+#
 
-# try 3 times for connection
-for i in {1..3}
-do
-    sleep 5
-    ucx_perftest -p ${PORT} -b test_types_ucp ${IP2} 2>&1
-    if [[ $? -eq 0 ]]; then
-        exit 0
-    fi
-done
+parse_ucx_result() {
+    perfsuit=(ucp_iov_contig_tag_lat ucp_iov_iov_tag_lat ucp_contig_contig_tag_lat ucp_iov_contig_tag_bw ucp_iov_iov_tag_bw ucp_contig_contig_tag_bw ucp_sync_tag_lat ucp_unexp_tag_lat ucp_wild_tag_lat ucp_stream_bw ucp_stream_lat ucp_put_lat ucp_put_bw ucp_get)
+}
 
-exit 1
-remotessh
+if [[ $? -eq 0]]; then
+    # get result from file, use stdout
+    parse_ucx_result ${OUTPUT_FILE}
+else
+    exit 1
+fi
