a80c25197d9d80989fd5063bed0c06cb1277fd80
monellz <monell.zhong@gmail.com>
2020-10-21 13:25:58 +0800

    perf_test: port

--- perf_v2_test.sh
+++ perf_v2_test.sh
@@ -4,9 +4,10 @@
 # IP2 as the server
 IP1=$1
 IP2=$2
+PORT=$3
 
-OUTPUT_DIR=$3
-TARGET_DIR=$4
+OUTPUT_DIR=$4
+TARGET_DIR=$5
 
 OUTPUT_FILE=${OUTPUT_DIR}/${IP1}-${IP2}.perf_v2.result
 
@@ -14,7 +15,7 @@ if [ -f ${OUTPUT_FILE} ]; then
     rm -rf ${OUTPUT_FILE}
 fi
 
-perfsuit=(RC UC UD)
+perfsuit=(ib_write_bw ib_write_lat)
 transtype=(RC)
 
 for case in ${perfsuit[@]}
@@ -41,7 +42,8 @@ do
         "source env_load.sh ${IP2} ${TARGET_DIR};" \
         "inner_case=$case;" \
         "inner_type=$type;" \
-        '$inner_case -x $V2_INDEX -d $DEV -c $inner_type --report_gbits;' \
+        "inner_port=${PORT};" \
+        '$inner_case -p $inner_port -x $V2_INDEX -d $DEV -c $inner_type --report_gbits;' \
         'exit' > /dev/null 2>&1 &
         
         
@@ -55,45 +57,13 @@ do
             "inner_case=$case;" \
             "inner_type=$type;" \
             "inner_serverip=$IP2;" \
-            '$inner_case $inner_serverip --report_gbits -F -x $V2_INDEX -d $DEV -c $inner_type;' \
-            'exit' > ${OUTPUT_FILE} 2>&1
+            "inner_port=${PORT};" \
+            '$inner_case $inner_serverip --report_gbits -F -p $inner_port -x $V2_INDEX -d $DEV -c $inner_type;' \
+            'exit' >> ${OUTPUT_FILE} 2>&1
 
             if [[ $? -eq 0 ]]; then
-                exit 0
+                break
             fi
         done
     done
 done
-
-
-
-# server side
-
-ssh ${IP2} \
-"cd ${TARGET_DIR};" \
-"source env_load.sh ${IP2} ${TARGET_DIR};" \
-'pkill ucx_perftest;' \
-'export UCX_TLS=rc;' \
-'export UCX_NET_DEVICES=${DEV}:${V2_PORT};' \
-'ucx_perftest -b test_types_ucp;' \
-'exit' > /dev/null 2>&1 &
-
-# client side
-
-ssh ${IP1} > ${OUTPUT_FILE} 2>/dev/null << remotessh
-
-cd ${TARGET_DIR}
-source env_load.sh ${IP1} ${TARGET_DIR}
-
-# try 3 times for connection
-for i in {1..3}
-do
-    sleep 5
-    ucx_perftest -b test_types_ucp ${IP2} 2>&1
-    if [[ $? -eq 0 ]]; then
-        exit 0
-    fi
-done
-
-exit 1
-remotessh
